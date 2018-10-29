7f329366d0dc52baa98a23c6de92fc691d464b61
Szymon Kopa <szymon@linkfactory.dk>
2018-10-29 23:35:53 +0100

    Add rubocop and resolve issues

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class EventsController < ApplicationController
-  def index()
+  def index
     @events = Event.all
   end
 
--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class HomePageController < ApplicationController
   def index
     @events = Event.limit(5)
