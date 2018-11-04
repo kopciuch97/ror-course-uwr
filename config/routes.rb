7c4cf11290be0fe3c732665a91b98fa921783454
Azoom <morenequs@gmail.com>
2018-11-04 17:37:23 +0100

    Add user profil Remove empty files

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users #, :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   root to: 'home_page#index'
 
   resources :events, only: %i[index show]
-  resources :users
+
+  namespace :user_panel, path: 'users' do
+    root to: 'profile#index'
+  end
+
+  # get '/users/profile', to: 'user_panel#index'
+  # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
