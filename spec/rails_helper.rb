81de6dba7ca13e30f3710753fd8ace73f9221fb4
Mariusz Bielecki <maniekb12@gmail.com>
2018-11-19 21:04:21 +0100

    [34] Add SimpleCov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'simplecov'
+  SimpleCov.start do
+    add_filter 'app/admin/'
+    add_filter 'spec/'
+    add_filter 'config/'
+  end
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
