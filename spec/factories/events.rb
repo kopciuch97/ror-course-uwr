658f5e9e5ecf6ba774415a3f433392e6374141f6
Mariusz Bielecki <maniekb12@gmail.com>
2018-11-20 02:58:13 +0100

    [33] Add more tests and factories

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -5,5 +5,8 @@ FactoryBot.define do
     name { Faker::Fallout.character }
     content { Faker::Fallout.quote }
     city { Faker::Fallout.location }
+    after :create do |event|
+      create_list :ticket, 3, event: event
+    end
   end
 end
