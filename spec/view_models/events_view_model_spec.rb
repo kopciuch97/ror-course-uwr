79dc50ab50187a40857af0344e60aec8ba39268c
Mariusz Bielecki <maniekb12@gmail.com>
2018-12-04 01:18:31 +0100

    [38] Improve tests stability

--- spec/view_models/events_view_model_spec.rb
+++ spec/view_models/events_view_model_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe EventsViewModel do
   let(:events) { FactoryBot.create_list(:event, 5) }
   describe '#events' do
     it do
-      expect(described_class.new.events(events[2].name).length).to eq(1)
+      described_class.new.events(events[2].name).each do |event|
+        expect(event.name).to eq(events[2].name)
+      end
     end
   end
 end
